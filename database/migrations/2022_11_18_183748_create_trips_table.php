4c481ad5b34aa39fd8343a06a17e31aa803466ac
Khaled Hossam <khaledhossam10@outlook.com>
2022-11-19 01:33:36 +0200

    create database structure, add migrations and models

--- database/migrations/2022_11_18_183748_create_trips_table.php
+++ database/migrations/2022_11_18_183748_create_trips_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('trips', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table-> foreignId('start_station_id')->constrained('stations')->cascadeOnDelete();
+            $table-> foreignId('end_station_id')->constrained('stations')->cascadeOnDelete();
+            $table-> foreignId('bus_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('trips');
     }
 };
--- database/migrations/2022_11_18_230331_create_reservations_table.php
+++ database/migrations/2022_11_18_230331_create_reservations_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->foreignId('seat_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('departure_stop_id')->constrained('stops');
+            $table->foreignId('arrival_stop_id')->constrained('stops');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 };
